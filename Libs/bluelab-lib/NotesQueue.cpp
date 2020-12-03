1ae2c100181f08ccb6418adc88a1ad6e132653e6
Niko Sound <contact@bluelab-plugs.com>
2020-12-03 17:40:53 +0100

    Envelope: first commit

---
+++ Libs/bluelab-lib/NotesQueue.cpp
@@ -0,0 +1,268 @@
+#include "NotesQueue.h"
+
+NotesQueue::NotesQueue(double sampleRate)
+{
+    mSampleRate = sampleRate;
+    
+    mId = 0;
+
+#if NQ_OPTIM1
+    mPadAmp = DBToAmp(GAIN_PAD);
+#endif
+    
+#if ADSR_SMOOTH_FEATURE
+    mADSRSmoothFactor = 0.0;
+#endif
+    
+#if ADSR_ADVANCED_PARAMS_FEATURE
+    mAttackPeak = 1.0;
+    mAttackShape = 1.0;
+    
+    mDecayPeak = 0.0;
+    mDecayShape = 1.0;
+    
+    mSustainDelay = 3.0;
+    mSustainShape = 1.0;
+    
+    mReleaseShape = 1.0;
+    
+    mEnd = 1.0;
+    mEndPeak = 0.0;
+    mEndShape = 1.0;
+#endif
+}
+
+NotesQueue::~NotesQueue()
+{
+    for (int i = 0; i < mNotes.size(); i++)
+    {
+        Note *note = mNotes[i];
+        delete note;
+    }
+}
+
+void
+NotesQueue::Reset()
+{
+    for (int i = 0; i < mNotes.size(); i++)
+    {
+        Note *note = mNotes[i];
+        delete note;
+    }
+    
+    mNotes.clear();
+}
+
+void
+NotesQueue::SetSampleRate(double sampleRate)
+{
+    mSampleRate = sampleRate;
+    
+    for (int i = 0; i < mNotes.size(); i++)
+    {
+        Note *note = mNotes[i];
+        note->mEnvGen.SetSampleRate(sampleRate);
+    }
+}
+
+void
+NotesQueue::AddNote(int keyNumber,
+                    double freq, double velocity,
+                    double a, double d, double s, double r)
+{
+    // ??
+    RemoveFinishedNotes();
+    
+    // Then add note
+    Note *newNote = new Note();
+    
+    newNote->mKeyNumber = keyNumber;
+    newNote->mEnvGen.SetSampleRate(mSampleRate);
+
+#if 1 // FIXED
+    newNote->mEnvGen.SetAttack(a);
+    newNote->mEnvGen.SetDecay(d);
+    newNote->mEnvGen.SetSustain(s);
+    newNote->mEnvGen.SetRelease(r);
+#endif
+    
+#if ADSR_ADVANCED_PARAMS_FEATURE
+    //newNote->mEnvGen.SetAttackPeak(mAttackPeak);
+    newNote->mEnvGen.SetAttackShape(mAttackShape);
+    
+    newNote->mEnvGen.SetDecayPeak(mDecayPeak);
+    newNote->mEnvGen.SetDecayShape(mDecayShape);
+    
+    newNote->mEnvGen.SetSustainDelay(mSustainDelay);
+    newNote->mEnvGen.SetSustainShape(mSustainShape);
+    
+    newNote->mEnvGen.SetReleaseShape(mReleaseShape);
+    
+    newNote->mEnvGen.SetEnd(mEnd);
+    newNote->mEnvGen.SetEndPeak(mEndPeak);
+    newNote->mEnvGen.SetEndShape(mEndShape);
+    
+    newNote->mEnvGen.SetAttackPeak(mAttackPeak); //
+#endif
+    
+    newNote->mEnvGen.EnterStage(EnvelopeGenerator3::ENVELOPE_STAGE_ATTACK);
+    
+    newNote->mVelocity = velocity;
+    
+#if ADSR_SMOOTH_FEATURE
+    newNote->mPrevEnvelope = 0.0;
+#endif
+    
+    mNotes.push_back(newNote);
+}
+
+void
+NotesQueue::ReleaseNote(int keyNumber)
+{
+    for (int i = 0; i < mNotes.size(); i++)
+    {
+        Note *note = mNotes[i];
+        
+        if (note->mKeyNumber == keyNumber)
+        {
+            if ((note->mEnvGen.GetCurrentStage() != EnvelopeGenerator3::ENVELOPE_STAGE_OFF) &&
+                (note->mEnvGen.GetCurrentStage() != EnvelopeGenerator3::ENVELOPE_STAGE_RELEASE))
+                note->mEnvGen.EnterStage(EnvelopeGenerator3::ENVELOPE_STAGE_RELEASE);
+        }
+    }
+}
+
+double
+NotesQueue::NextSample()
+{
+    double samp = 0.0;
+    
+    int numNotes = mNotes.size();
+    for (int i = 0; i < numNotes; i++)
+    {
+        Note *note = mNotes[i];
+        
+        // Synth of the current ENVELOPE VALUE        
+        double env = note->mEnvGen.NextSample();
+        
+#if ADSR_SMOOTH_FEATURE
+        double newEnv = (1.0 - mADSRSmoothFactor)*env + mADSRSmoothFactor*note->mPrevEnvelope;
+        note->mPrevEnvelope = newEnv;
+        
+        env = newEnv;
+#endif
+        
+        double gain = env*note->mVelocity;
+        
+#if 1 // Gain pad
+        
+#if !NQ_OPTIM1
+        double padAmp = DBToAmp(GAIN_PAD);
+#else
+        double padAmp = mPadAmp;
+#endif
+        
+        gain *= padAmp;
+#endif
+
+#if !DEBUG_DUMP_ENVELOPES
+        samp += gain;
+#else
+        samp += newEnv;
+#endif
+    }
+    
+    return samp;
+}
+
+void
+NotesQueue::Update()
+{
+    RemoveFinishedNotes();
+}
+
+#if ADSR_SMOOTH_FEATURE
+void
+NotesQueue::SetADSRSmoothFactor(double smoothFactor)
+{
+    mADSRSmoothFactor = smoothFactor;
+}
+#endif
+
+#if ADSR_ADVANCED_PARAMS_FEATURE
+void
+NotesQueue::SetAttackPeak(double gain)
+{
+    mAttackPeak = gain;
+}
+
+void
+NotesQueue::SetAttackShape(double shape)
+{
+    mAttackShape = shape;
+}
+
+void
+NotesQueue::SetDecayPeak(double peak)
+{
+    mDecayPeak = peak;
+}
+
+void
+NotesQueue::SetDecayShape(double shape)
+{
+    mDecayShape = shape;
+}
+
+void
+NotesQueue::SetSustainDelay(double delay)
+{
+    mSustainDelay = delay;
+}
+
+void
+NotesQueue::SetSustainShape(double shape)
+{
+    mSustainShape = shape;
+}
+
+void
+NotesQueue::SetReleaseShape(double shape)
+{
+    mReleaseShape = shape;
+}
+
+void
+NotesQueue::SetEnd(double end)
+{
+    mEnd = end;
+}
+
+void
+NotesQueue::SetEndPeak(double endPeak)
+{
+    mEndPeak = endPeak;
+}
+
+void
+NotesQueue::SetEndShape(double endShape)
+{
+    mEndShape = endShape;
+}
+#endif
+
+void
+NotesQueue::RemoveFinishedNotes()
+{
+    // First, remove finished notes
+    vector<Note *> newNotes;
+    
+    for (int i = 0; i < mNotes.size(); i++)
+    {
+        Note *note = mNotes[i];
+        if (note->mEnvGen.GetCurrentStage() != EnvelopeGenerator3::ENVELOPE_STAGE_OFF)
+            newNotes.push_back(note);
+    }
+    
+    mNotes = newNotes;
+}
---
+++ Libs/bluelab-lib/NotesQueue.h
@@ -0,0 +1,100 @@
+#ifndef NOTES_QUEUE_H
+#define NOTES_QUEUE_H
+
+#define ADSR_SMOOTH_FEATURE 1
+
+#define ADSR_ADVANCED_PARAMS_FEATURE 1
+
+#define NQ_OPTIM1 1
+
+class NotesQueue
+{
+public:
+    struct Note
+    {
+        int mKeyNumber;
+        
+        EnvelopeGenerator3 mEnvGen;
+        
+        double mVelocity;
+        
+#if ADSR_SMOOTH_FEATURE
+        double mPrevEnvelope;
+#endif
+    };
+    
+    NotesQueue(double sampleRate);
+    
+    virtual ~NotesQueue();
+    
+    void Reset();
+    
+    void SetSampleRate(double sampleRate);
+    
+    void AddNote(int keyNumber,
+                 double freq, double velocity,
+                 double a, double d, double s, double r);
+    
+    void ReleaseNote(int keyNumber);
+    
+    double NextSample();
+    
+    void Update();
+    
+#if ADSR_SMOOTH_FEATURE
+    void SetADSRSmoothFactor(double smoothFactor);
+#endif
+    
+#if ADSR_ADVANCED_PARAMS_FEATURE
+    void SetAttackPeak(double gain);
+    void SetAttackShape(double shape);
+
+    void SetDecayPeak(double peak);
+    void SetDecayShape(double shape);
+    
+    void SetSustainDelay(double delay);
+    void SetSustainShape(double shape);
+    
+    void SetReleaseShape(double shape);
+    
+    void SetEnd(double end);
+    void SetEndPeak(double endPeak);
+    void SetEndShape(double endShape);
+#endif
+    
+protected:
+    void RemoveFinishedNotes();
+    
+    double mSampleRate;
+    
+    vector<Note *> mNotes;
+    
+    unsigned long long mId;
+    
+#if NQ_OPTIM1
+    double mPadAmp;
+#endif
+    
+#if ADSR_SMOOTH_FEATURE
+    double mADSRSmoothFactor;
+#endif
+    
+#if ADSR_ADVANCED_PARAMS_FEATURE
+    double mAttackPeak;
+    double mAttackShape;
+    
+    double mDecayPeak;
+    double mDecayShape;
+    
+    double mSustainDelay;
+    double mSustainShape;
+    
+    double mReleaseShape;
+
+    double mEnd;
+    double mEndPeak;
+    double mEndShape;
+#endif
+};
+
+#endif
