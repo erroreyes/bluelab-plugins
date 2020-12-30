6902f70579aa77da63ef518b14aac11fcd47d663
Niko Sound <contact@bluelab-plugs.com>
2020-12-30 08:54:02 +0100

    SpectroExpe: - IMPROV: GhostViewerFftObj => SpectroExpeFftObj

---
+++ Libs/bluelab-lib/SpectroExpeFftObj.h
@@ -0,0 +1,58 @@
+//
+//  GhostViewerFftObj.h
+//  BL-GhostViewer
+//
+//  Created by Pan on 02/06/18.
+//
+//
+
+#ifndef __BL_GhostViewer__GhostViewerFftObj__
+#define __BL_GhostViewer__GhostViewerFftObj__
+
+#include <BLTypes.h>
+
+#include <FftProcessObj16.h>
+
+// From ChromaFftObj
+//
+
+// SpectrogramDisplayScroll => SpectrogramDisplayScroll3
+
+class BLSpectrogram4;
+class SpectrogramDisplayScroll3;
+class GhostViewerFftObj : public ProcessObj
+{
+public:
+    GhostViewerFftObj(int bufferSize, int oversampling, int freqRes,
+                      BL_FLOAT sampleRate);
+    
+    virtual ~GhostViewerFftObj();
+    
+    void ProcessFftBuffer(WDL_TypedBuf<WDL_FFT_COMPLEX> *ioBuffer,
+                          const WDL_TypedBuf<WDL_FFT_COMPLEX> *scBuffer = NULL);
+    
+    void Reset(int bufferSize, int oversampling, int freqRes, BL_FLOAT sampleRate);
+    
+    BLSpectrogram4 *GetSpectrogram();
+    
+    void SetSpectrogramDisplay(SpectrogramDisplayScroll3 *spectroDisplay);
+    
+    void SetSpeedMod(int speedMod);
+    
+protected:
+    void AddSpectrogramLine(const WDL_TypedBuf<BL_FLOAT> &magns,
+                            const WDL_TypedBuf<BL_FLOAT> &phases);
+
+    //
+    BLSpectrogram4 *mSpectrogram;
+    
+    SpectrogramDisplayScroll3 *mSpectroDisplay;
+    
+    long mLineCount;
+    
+    deque<WDL_TypedBuf<BL_FLOAT> > mOverlapLines;
+    
+    int mSpeedMod;
+};
+
+#endif /* defined(__BL_GhostViewer__GhostViewerFftObj__) */
