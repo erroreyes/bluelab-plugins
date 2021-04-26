89e862615326a65aa09e3652b1738fbcebc7a2fa
Niko Sound <contact@bluelab-plugs.com>
2021-04-27 00:32:26 +0200

    Infra: - REFACT: big code clean in SineSynth3

--- Libs/bluelab-lib/SineSynth3.h
+++ Libs/bluelab-lib/SineSynth3.h
@@ -50,38 +50,15 @@ public:
     // Debug
     void SetDebug(bool flag);
     
-protected:
-#if 0
-    void ComputeSamplesWin(WDL_TypedBuf<BL_FLOAT> *samples);
-    
-    void ComputeSamplesPartials(WDL_TypedBuf<BL_FLOAT> *samples);
-    
-    // Optim
-    void ComputeSamplesSAS5(WDL_TypedBuf<BL_FLOAT> *samples);
-    
-    void ComputeSamplesSASTable(WDL_TypedBuf<BL_FLOAT> *samples);
-    void ComputeSamplesSASTable2(WDL_TypedBuf<BL_FLOAT> *samples); // Better interpolation
-    void ComputeSamplesSASOverlap(WDL_TypedBuf<BL_FLOAT> *samples);
-#endif
-    
+protected:    
     // Compute steps
     //
-
-#if 0
-    // Versions to interpolate over time
-    bool FindPartial(BL_FLOAT freq);
-
-    void GetPartial(PartialTracker5::Partial *result, int index, BL_FLOAT t);
-#endif
     
     int FindPrevPartialIdx(int currentPartialIdx);
 
     int FindPartialFromIdx(int partialIdx);
 
-    
-#if 0
-    void GetPartial(Partial *result, int index, BL_FLOAT t);
-#endif
+    //
     
     int mBufferSize;
     BL_FLOAT mSampleRate;
