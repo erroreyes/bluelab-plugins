07ec447c69e4de540f27e6933828284cd674c3d0
Niko Sound <contact@bluelab-plugs.com>
2021-07-15 16:17:32 +0200

    LoFi: - IMPROV: same curve display whatever the block size

--- Libs/bluelab-lib/Bufferizer.cpp
+++ Libs/bluelab-lib/Bufferizer.cpp
@@ -20,8 +20,9 @@ Bufferizer::GetBuffer(WDL_TypedBuf<BL_FLOAT> *buffer)
 {
   if (mValues.GetSize() < mBufferSize)
     return false;
-  
-  buffer->Add(mValues.Get(), mBufferSize);
+
+  //buffer->Add(mValues.Get(), mBufferSize);
+  BLUtils::CopyBuf(buffer, mValues.Get(), mBufferSize);
   
   BLUtils::ConsumeLeft(&mValues, mBufferSize);
   
