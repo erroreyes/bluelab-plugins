c5f921a7aafecc20b1ea1a03c6ceeaf54d597505
Niko Sound <contact@bluelab-plugs.com>
2021-08-10 17:36:11 +0200

    OpenBLAS dynamic arch for Linux

--- Libs/OpenBLAS.cmake
+++ Libs/OpenBLAS.cmake
@@ -4,5 +4,6 @@ set(BLUELAB_DEPS ${IPLUG2_DIR}/BL-Dependencies)
 add_library(_OpenBLAS INTERFACE)
 iplug_target_add(_OpenBLAS INTERFACE
   INCLUDE ${BLUELAB_DEPS}/OpenBLAS
-  LINK ${BLUELAB_DEPS}/OpenBLAS/libopenblas_sandybridge-r0.3.9_linux.a
+  #LINK ${BLUELAB_DEPS}/OpenBLAS/libopenblas_sandybridge-r0.3.9_linux.a
+  LINK ${BLUELAB_DEPS}/OpenBLAS/libopenblas_dynamic-r0.3.9_linux_st.a
 )
