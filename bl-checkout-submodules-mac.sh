3d5536cec083b2dee418015ae2cb6b9f831fcec4
Niko Sound <contact@bluelab-plugs.com>
2021-08-20 12:06:22 +0200

    Script checkout submodules: new script for mac

---
+++ bl-checkout-submodules-mac.sh
@@ -0,0 +1,145 @@
+#!/bin/sh
+
+# bluelabpxx@ssh.cluster026.hosting.ovh.net:bluelab.git
+
+# git submodules init
+# git submodules update
+
+git pull --recurse-submodules
+
+cd bl-darknet
+git checkout master
+git pull origin master
+
+cd ..
+cd BL-Plugins
+git checkout master
+git pull origin master
+
+cd ..
+cd BuildSystem
+git checkout master
+git pull origin master
+
+cd ..
+cd iPlug2
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd TestSystem-mac
+git checkout master
+git pull origin master
+
+cd ../Libs
+cd CrossoverFilter
+git checkout master
+git pull origin master
+
+cd ..
+cd darknet
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd Decimator
+git checkout master
+git pull origin master
+
+cd ..
+cd fastapprox
+git checkout master
+git pull origin master
+
+cd ..
+cd fast-math
+git checkout master
+git pull origin master
+
+cd ..
+cd flac
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd fmem
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd freeverb
+git checkout master
+git pull origin master
+
+cd ..
+cd glm
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd ini_parser
+git checkout master
+git pull origin master
+
+cd ..
+cd KalmanFilter
+git checkout master
+git pull origin master
+
+cd ..
+cd libdct
+git checkout master
+git pull origin master
+
+cd ..
+cd libebur128
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd libsamplerate/
+git checkout master
+git pull origin master
+
+cd ..
+cd libsndfile
+git checkout master
+git pull origin master
+
+cd ..
+cd OpenBLAS
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd PhaseVocoder-DSP
+git checkout master
+git pull origin master
+
+cd ..
+cd r8brain
+git checkout master
+git pull origin master
+
+cd ..
+cd RandomSequence
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd RTConvolve
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd smbPitchShift
+git checkout master
+git pull origin master
+
+cd ..
+cd sndfilter
+git checkout bluelab
+git pull origin bluelab
+
+cd ..
+cd ..
