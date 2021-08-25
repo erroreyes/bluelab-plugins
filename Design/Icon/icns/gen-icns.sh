142b1a435f561edcbcf792af8cff03c7e9ce1b6d
Niko Sound <contact@bluelab-plugs.com>
2021-08-25 14:22:06 +0200

    Design: Generated .icns with new logo (on linux)

---
+++ Design/Icon/icns/gen-icns.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#png2icns BL-Plugin.icns icon_512x512@2x.png icon_512x512.png icon_256x256@2x.png icon_256x256.png icon_128x128@2x.png icon_128x128.png icon_32x32@2x.png icon_32x32.png icon_16x16@2x.png icon_16x16.png
+
+png2icns BL-Plugin.icns icon_512x512.png icon_256x256.png icon_128x128.png icon_32x32.png icon_16x16.png
