ee6cca0e93cd5396e7b5c0024cbc60580736534b
Stinky Nibbler <nibbler@707lulz-wintermute>
2022-06-27 14:55:56 +0200

    install script

--- bl-checkout-submodules.sh
+++ bl-checkout-submodules.sh
@@ -7,59 +7,43 @@
 
 git pull --recurse-submodules
 
-cd bl-darknet
-git checkout master
-git pull ovh master
-
-cd ..
-cd BL-Plugins
-git checkout master
-git pull ovh master
-
-cd ..
-cd BuildSystem
-git checkout master
-git pull ovh master
-
 cd ..
 cd iPlug2
+git remote rm origin
+git remote add origin https://github.com/deadlab-plugins/iPlug2.git 
+git pull origin bluelab
 git checkout bluelab
-git pull ovh bluelab
-
-cd ..
-cd TestSystem-mac
-git checkout master
-git pull origin master
+git pull origin bluelab
 
 cd ../Libs
 cd CrossoverFilter
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd darknet
 git checkout bluelab
-git pull ovh bluelab
+git pull origin bluelab
 
 cd ..
 cd Decimator
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd fastapprox
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd fast-math
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd flac
 git checkout bluelab
-git pull ovh bluelab
+git pull origin bluelab
 
 cd ..
 cd fmem
@@ -69,7 +53,7 @@ git pull origin bluelab
 cd ..
 cd freeverb
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd glm
@@ -79,17 +63,17 @@ git pull origin bluelab
 cd ..
 cd ini_parser
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd KalmanFilter
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd libdct
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd libebur128
@@ -99,27 +83,27 @@ git pull origin bluelab
 cd ..
 cd libsamplerate/
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd libsndfile
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd OpenBLAS
 git checkout bluelab
-git pull ovh bluelab
+git pull origin bluelab
 
 cd ..
 cd PhaseVocoder-DSP
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd r8brain
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd RandomSequence
@@ -134,7 +118,7 @@ git pull origin bluelab
 cd ..
 cd smbPitchShift
 git checkout master
-git pull ovh master
+git pull origin master
 
 cd ..
 cd sndfilter
