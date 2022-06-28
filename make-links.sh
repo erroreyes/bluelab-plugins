fb6a52e7a3d547010612f926b7e92d9f5718d48a
Stinky Nibbler <nibbler@707lulz-wintermute>
2022-06-28 14:27:07 +0200

    make-links script

---
+++ make-links.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+cd iPlug2/Examples
+
+ln -s ../../BL-Plugins/BL-Air ./BL-Air
+
+ln -s ../../BL-Plugins/BL-AutoGain ./BL-AutoGain
+ln -s ../../BL-Plugins/BL-Sine ./BL-Sine
+ln -s ../../BL-Plugins/BL-Bat ./BL-Bat
+ln -s ../../BL-Plugins/BL-LoFi ./BL-LoFi
+ln -s ../../BL-Plugins/BL-SoundMetaViewer ./BL-SoundMetaViewer
+ln -s ../../BL-Plugins/BL-Chroma ./BL-Chroma
+ln -s ../../BL-Plugins/BL-Morpho ./BL-Morpho
+ln -s ../../BL-Plugins/BL-Spatializer ./BL-Spatializer
+ln -s ../../BL-Plugins/BL-Denoiser ./BL-Denoiser
+ln -s ../../BL-Plugins/BL-NoiseRemover ./BL-NoiseRemover
+ln -s ../../BL-Plugins/BL-SpectralDiff ./BL-SpectralDiff
+ln -s ../../BL-Plugins/BL-DUET ./BL-DUET
+ln -s ../../BL-Plugins/BL-OnsetDetect ./BL-OnsetDetect
+ln -s ../../BL-Plugins/BL-SpectroExpe ./BL-SpectroExpe
+ln -s ../../BL-Plugins/BL-Envelope ./BL-Envelope
+ln -s ../../BL-Plugins/BL-Panogram ./BL-Panogram
+ln -s ../../BL-Plugins/BL-SpectrumView ./BL-SpectrumView
+ln -s ../../BL-Plugins/BL-EQHack ./BL-EQHack
+ln -s ../../BL-Plugins/BL-PitchShift ./BL-PitchShift
+ln -s ../../BL-Plugins/BL-StereoDeReverb ./BL-StereoDeReverb
+ln -s ../../BL-Plugins/BL-Gain12 ./BL-Gain12
+ln -s ../../BL-Plugins/BL-Precedence ./BL-Precedence
+ln -s ../../BL-Plugins/BL-StereoPan ./BL-StereoPan
+ln -s ../../BL-Plugins/BL-Gain200 ./BL-Gain200
+ln -s ../../BL-Plugins/BL-Rebalance ./BL-Rebalance
+ln -s ../../BL-Plugins/BL-StereoWidth ./BL-StereoWidth
+ln -s ../../BL-Plugins/BL-Gain24 ./BL-Gain24
+ln -s ../../BL-Plugins/BL-RebalanceStereo ./BL-RebalanceStereo
+ln -s ../../BL-Plugins/BL-Wav3s ./BL-Wav3s
+ln -s ../../BL-Plugins/BL-Gain60 ./BL-Gain60
+ln -s ../../BL-Plugins/BL-Reverb ./BL-Reverb
+ln -s ../../BL-Plugins/BL-Ghost ./BL-Ghost
+ln -s ../../BL-Plugins/BL-ReverbDepth ./BL-ReverbDepth
+ln -s ../../BL-Plugins/BL-GhostViewer ./BL-GhostViewer
+ln -s ../../BL-Plugins/BL-SampleDelay ./BL-SampleDelay
+ln -s ../../BL-Plugins/BL-Impulses ./BL-Impulses
+ln -s ../../BL-Plugins/BL-SASViewer ./BL-SASViewer
+ln -s ../../BL-Plugins/BL-Infra ./BL-Infra
+ln -s ../../BL-Plugins/BL-Saturate ./BL-Saturate
+
+cd ../..
